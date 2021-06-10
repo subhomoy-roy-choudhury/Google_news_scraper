e3b9df34d006665bd29501bb543e0a81398b0525
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2021-06-10 22:06:57 +0530

    deploy

--- scraper/migrations/0001_initial.py
+++ scraper/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('description', models.TextField()),
-                ('details', models.CharField(max_length=100)),
+                ('details', models.CharField(max_length=40)),
                 ('date_time', models.DateTimeField()),
                 ('image_url', models.URLField()),
             ],
