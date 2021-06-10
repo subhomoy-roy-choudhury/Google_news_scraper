71af9ae23fd62570d495bb800f1ed0f4a63a593f
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2021-06-10 21:18:56 +0530

    prepare to deploy

--- main/settings.py
+++ main/settings.py
@@ -70,8 +70,7 @@ TEMPLATES = [
     },
 ]
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+
 
 WSGI_APPLICATION = 'main.wsgi.application'
 
@@ -129,3 +128,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
