4b641de835a24e32ff05937e886494aa15513e73
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2021-06-10 18:34:20 +0530

    Initial Commit

---
+++ google_news_scraper.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+from random import randint
+
+r = requests.get("https://news.google.com/search?q=nri&hl=en-IN&gl=IN&ceid=IN%3Aen")
+print(r.status_code)  
+content = r.text
+soup = BeautifulSoup(content, "html.parser")
+
+st_divs1 = soup.find_all('div',{"class" : "NiLAwe y6IFtc R7GTQ keNKEd j7vNaf nID9nc"})
+print(len(st_divs1))
+
+for i in range(len(st_divs1)) :
+    print('*'*40)
+    st_divs = soup.find_all('div',{"class" : "NiLAwe y6IFtc R7GTQ keNKEd j7vNaf nID9nc"})[i]
+    detail = st_divs.find('h3',{"class" : "ipQwMb ekueJc RD0gLb"}).text
+    description = st_divs.find('span',{"class" : "xBbh9"}).text
+    date_time = st_divs.find('time',{"class" : "WW6dff uQIVzc Sksgp"})['datetime']
+    image = 'https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg'
+    try:
+        image_url = st_divs.find('img',{"class" : "tvs3Id QwxBBf"})['src']
+    except Exception as e:
+        print(e)
+        pass
+
+    print(detail,description,date_time,image_url)
+    print('*'*40)
